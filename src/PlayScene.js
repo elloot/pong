aea8cf4b9a407d88b1cb4bbd1f01b2d9ed4407e8
elloot <elliot.duchek@gmail.com>
2021-09-08 11:19:31 +0200

    ✨ add collision between ball and paddles ✨ make paddles immovable

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -41,7 +41,8 @@ export default class PlayScene extends Phaser.Scene {
     );
     this.physics.add
       .existing(this.playerPaddle)
-      .body.setCollideWorldBounds(true);
+      .body.setCollideWorldBounds(true)
+      .setImmovable(true);
 
     this.AIPaddle = this.add.rectangle(
       width - gameOptions.paddleWidth / 2,
@@ -50,7 +51,14 @@ export default class PlayScene extends Phaser.Scene {
       gameOptions.paddleHeight,
       0x000000
     );
-    this.physics.add.existing(this.AIPaddle).body.setCollideWorldBounds(true);
+    this.physics.add
+      .existing(this.AIPaddle)
+      .body.setCollideWorldBounds(true)
+      .setImmovable(true);
+
+    this.paddles = [this.playerPaddle, this.AIPaddle];
+
+    this.physics.add.collider(ball, this.paddles);
 
     this.keys = this.input.keyboard.addKeys('W, S, up, down');
   }
