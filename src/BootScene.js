f3ed1a7988299efc987aa5ffb857c54ea8ac2ecc
elloot <elliot.duchek@gmail.com>
2021-09-08 10:51:13 +0200

    ✨ switch directly to play scene instead of going through menu scene

--- src/BootScene.js
+++ src/BootScene.js
@@ -18,8 +18,8 @@ export default class BootScene extends Phaser.Scene {
   }
 
   update() {
-    this.scene.start('menu');
-    // this.scene.start('play');
-    // this.scene.remove();
+    // this.scene.start('menu');
+    this.scene.start('play');
+    this.scene.remove();
   }
 }
