ec8a2851c88f7111edb81e2f2c08be516d5d531a
elloot <elliot.duchek@gmail.com>
2021-09-09 16:36:36 +0200

    🔧 decrease AI paddle speed because of new fancy exponential function

--- src/gameOptions.js
+++ src/gameOptions.js
@@ -3,7 +3,7 @@ export default {
   ballMaxVelocity: 700,
   ballRadius: 10,
   paddleSpeed: 600,
-  AIPaddleSpeed: 50000,
+  AIPaddleSpeed: 600,
   paddleWidth: 10,
   paddleHeight: 100,
   scoreFontSize: 50
