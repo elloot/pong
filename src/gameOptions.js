d51dc22841493ea825fa2b57b8b292d7df97228e
elloot <elliot.duchek@gmail.com>
2021-09-09 14:16:51 +0200

    🔧 make the ball and paddles smaller, add key for AI paddle speed

--- src/gameOptions.js
+++ src/gameOptions.js
@@ -1,9 +1,10 @@
 export default {
   startingVelocity: 300,
   ballMaxVelocity: 700,
-  ballRadius: 40,
+  ballRadius: 10,
   paddleSpeed: 600,
-  paddleWidth: 30,
-  paddleHeight: 200,
+  AIPaddleSpeed: 10000,
+  paddleWidth: 10,
+  paddleHeight: 100,
   scoreFontSize: 50
 };
